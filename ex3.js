15d62894aa4050e0e929abb16de01ebed1c80164
KoRA-Ai <91049653+KoRA-Ai@users.noreply.github.com>
2022-11-25 04:31:08 +0800

    Update ex3.js

--- ex3.js
+++ ex3.js
@@ -5,8 +5,9 @@ var app = new Vue({
         newtodo:''
     },
     methods:{
-        addTodo: function(todo){
-            this.todolist.push({content:todo,completed:false})
+        addTodo: function(newtodo){
+            if(newtodo!='')
+                this.todolist.push({content:newtodo,completed:false});
         },
         deleteTodo: function(todo){
             this.todolist.splice(this.todolist.indexOf(todo),1)
